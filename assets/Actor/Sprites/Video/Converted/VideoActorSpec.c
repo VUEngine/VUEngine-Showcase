a5116f2f37ab68dc49141939d4315441e81eb52e
Christian Radke <c.radke@posteo.de>
2025-02-07 21:23:57 +0100

    Further shrink video

--- assets/Actor/Sprites/Video/Converted/VideoActorSpec.c
+++ assets/Actor/Sprites/Video/Converted/VideoActorSpec.c
@@ -15,12 +15,12 @@
 // DECLARATIONS
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-extern uint32 VideoActorVideoL058LTiles[];
-extern uint16 VideoActorVideoL058LMap[];
-extern uint32 VideoActorVideoL058LTilesFrameOffsets[];
-extern uint32 VideoActorVideoR058RTiles[];
-extern uint16 VideoActorVideoR058RMap[];
-extern uint32 VideoActorVideoR058RTilesFrameOffsets[];
+extern uint32 VideoActorVideoL064LTiles[];
+extern uint16 VideoActorVideoL064LMap[];
+extern uint32 VideoActorVideoL064LTilesFrameOffsets[];
+extern uint32 VideoActorVideoR064RTiles[];
+extern uint16 VideoActorVideoR064RMap[];
+extern uint32 VideoActorVideoR064RTilesFrameOffsets[];
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // ANIMATIONS
@@ -29,38 +29,7 @@ extern uint32 VideoActorVideoR058RTilesFrameOffsets[];
 AnimationFunctionROMSpec VideoAnimation1AnimationSpec =
 {
 	// Number of frames that the texture supports of this animation function
-	120,
-
-	// Frames to play in animation
-	{
-		0, 0, 2, 2, 4, 4, 6, 6, 8, 8, 
-		10, 10, 12, 12, 14, 14, 16, 16, 18, 18, 
-		20, 20, 22, 22, 24, 24, 26, 26, 28, 28, 
-		30, 30, 32, 32, 34, 34, 36, 36, 38, 38, 
-		40, 40, 42, 42, 44, 44, 46, 46, 48, 48, 
-		50, 50, 52, 52, 54, 54, 56, 56, 58, 58, 
-		60, 60, 62, 62, 64, 64, 66, 66, 68, 68, 
-		70, 70, 72, 72, 74, 74, 76, 76, 78, 78, 
-		80, 80, 82, 82, 84, 84, 86, 86, 88, 88, 
-		90, 90, 92, 92, 94, 94, 96, 96, 98, 98, 
-		100, 100, 102, 102, 104, 104, 106, 106, 108, 108, 
-		110, 110, 112, 112, 114, 114, 116, 116, 118, 118, 
-	},
-
-	// Number of cycles a frame of animation is displayed
-	1,
-
-	// Whether to play it in loop or not
-	true,
-
-	// Animation's name
-	"4Color",
-};
-
-AnimationFunctionROMSpec VideoAnimation2AnimationSpec =
-{
-	// Number of frames that the texture supports of this animation function
-	120,
+	114,
 
 	// Frames to play in animation
 	{
@@ -75,7 +44,7 @@ AnimationFunctionROMSpec VideoAnimation2AnimationSpec =
 		80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 
 		90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 
 		100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 
-		110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 
+		110, 111, 112, 113, 
 	},
 
 	// Number of cycles a frame of animation is displayed
@@ -86,6 +55,37 @@ AnimationFunctionROMSpec VideoAnimation2AnimationSpec =
 
 	// Animation's name
 	"FrameBlend",
+};
+
+AnimationFunctionROMSpec VideoAnimation2AnimationSpec =
+{
+	// Number of frames that the texture supports of this animation function
+	114,
+
+	// Frames to play in animation
+	{
+		0, 0, 2, 2, 4, 4, 6, 6, 8, 8, 
+		10, 10, 12, 12, 14, 14, 16, 16, 18, 18, 
+		20, 20, 22, 22, 24, 24, 26, 26, 28, 28, 
+		30, 30, 32, 32, 34, 34, 36, 36, 38, 38, 
+		40, 40, 42, 42, 44, 44, 46, 46, 48, 48, 
+		50, 50, 52, 52, 54, 54, 56, 56, 58, 58, 
+		60, 60, 62, 62, 64, 64, 66, 66, 68, 68, 
+		70, 70, 72, 72, 74, 74, 76, 76, 78, 78, 
+		80, 80, 82, 82, 84, 84, 86, 86, 88, 88, 
+		90, 90, 92, 92, 94, 94, 96, 96, 98, 98, 
+		100, 100, 102, 102, 104, 104, 106, 106, 108, 108, 
+		110, 110, 112, 112, 
+	},
+
+	// Number of cycles a frame of animation is displayed
+	1,
+
+	// Whether to play it in loop or not
+	true,
+
+	// Animation's name
+	"4Color",
 };
 
 AnimationFunctionROMSpec* VideoAnimationSpecs[] =
@@ -111,10 +111,10 @@ CharSetROMSpec VideoSprite1LCharsetSpec =
 	true,
 
 	// Tiles array
-	VideoActorVideoL058LTiles,
+	VideoActorVideoL064LTiles,
 
 	// Frame offsets array
-	VideoActorVideoL058LTilesFrameOffsets
+	VideoActorVideoL064LTilesFrameOffsets
 };
 
 TextureROMSpec VideoSprite1LTextureSpec =
@@ -123,7 +123,7 @@ TextureROMSpec VideoSprite1LTextureSpec =
 	(CharSetSpec*)&VideoSprite1LCharsetSpec,
 
 	// Pointer to the map array that defines how to use the tiles from the char set
-	VideoActorVideoL058LMap,
+	VideoActorVideoL064LMap,
 
 	// Horizontal size in tiles of the texture (max. 64)
 	48,
@@ -201,10 +201,10 @@ CharSetROMSpec VideoSprite1RCharsetSpec =
 	true,
 
 	// Tiles array
-	VideoActorVideoR058RTiles,
+	VideoActorVideoR064RTiles,
 
 	// Frame offsets array
-	VideoActorVideoR058RTilesFrameOffsets
+	VideoActorVideoR064RTilesFrameOffsets
 };
 
 TextureROMSpec VideoSprite1RTextureSpec =
@@ -213,7 +213,7 @@ TextureROMSpec VideoSprite1RTextureSpec =
 	(CharSetSpec*)&VideoSprite1RCharsetSpec,
 
 	// Pointer to the map array that defines how to use the tiles from the char set
-	VideoActorVideoR058RMap,
+	VideoActorVideoR064RMap,
 
 	// Horizontal size in tiles of the texture (max. 64)
 	48,
@@ -311,6 +311,6 @@ ActorROMSpec VideoActorSpec =
 	kTypeNone,
 
 	// Animation to play automatically
-	"4Color"
+	"FrameBlend"
 	
 };
