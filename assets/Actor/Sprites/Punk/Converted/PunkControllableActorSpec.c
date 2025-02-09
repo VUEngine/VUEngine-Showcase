889da7e4796d027c1d1bfb6707def2455c73311a
Jorge Eremiev <jorgech3@gmail.com>
2025-02-09 11:38:30 -0500

    Fixed comments.

--- assets/Actor/Sprites/Punk/Converted/PunkControllableActorSpec.c
+++ assets/Actor/Sprites/Punk/Converted/PunkControllableActorSpec.c
@@ -141,7 +141,7 @@ BgmapSpriteROMSpec PunkControllableSprite1SpriteSpec =
 	// Flag to indicate in which display to show the texture (__WORLD_ON, __WORLD_LON or __WORLD_RON)
 	__WORLD_ON,
 
-	// The display mode (__WORLD_BGMAP, __WORLD_AFFINE, __WORLD_OBJECT or __WORLD_HBIAS)
+	// The display mode (__WORLD_BGMAP, __WORLD_AFFINE or __WORLD_HBIAS)
 	__WORLD_AFFINE,
 	
 	// Pointer to affine/hbias manipulation function
@@ -231,7 +231,7 @@ BgmapSpriteROMSpec PunkControllableSprite2SpriteSpec =
 	// Flag to indicate in which display to show the texture (__WORLD_ON, __WORLD_LON or __WORLD_RON)
 	__WORLD_ON,
 
-	// The display mode (__WORLD_BGMAP, __WORLD_AFFINE, __WORLD_OBJECT or __WORLD_HBIAS)
+	// The display mode (__WORLD_BGMAP, __WORLD_AFFINE or __WORLD_HBIAS)
 	__WORLD_AFFINE,
 	
 	// Pointer to affine/hbias manipulation function
