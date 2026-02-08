736b84b985b10a459269f0a5712bf0b18cac6569
Christian Radke <c.radke@posteo.de>
2026-02-08 15:01:55 +0100

    Fixed German translation

--- assets/Languages.c
+++ assets/Languages.c
@@ -188,7 +188,7 @@ const char* const deLanguageStrings[] =
     /* kStringActorDestructionLabel: */
     "Akteur-Destruktoren",
     /* kStringActorsLabel: */
-    "Akteur",
+    "Akteure",
     /* kStringAnimationsLabel: */
     "Animationen",
     /* kStringAnimationSubtitle: */
