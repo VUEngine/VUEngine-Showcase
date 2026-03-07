46a3cbae650d493556d64f0288841da22c444467
Jorge Eremiev <jorgech3@gmail.com>
2026-03-06 23:08:25 -0500

    Fixed warning.

--- source/Controllers/Graphics/Sprites/BgmapSpriteExtensions.c
+++ source/Controllers/Graphics/Sprites/BgmapSpriteExtensions.c
@@ -56,8 +56,9 @@ static int16 BgmapSprite::applyHBias(BgmapSprite bgmapSprite, int16 waveHBiasThr
 {
 	if(__TOTAL_WORLD_LAYERS <= (uint16)bgmapSprite->index)
 	{
-		return;
+		return 0;
 	}
+
 	// Look up table offset
 	static uint8 step = 0;
 	step = (step < ((HBIAS_WAVE_LUT_ENTRIES << waveHBiasThrottle) - 1)) ? step + 1 : 0;
